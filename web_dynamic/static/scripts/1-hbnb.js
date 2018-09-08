acdbf06ce0333d10d2b864e3c455d45778b6b174
Lisa Olson <olson.lisa94@gmail.com>
2018-09-08 02:05:34 +0000

    pushes checked attributes into a list and prints into h4 tag

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -3,14 +3,13 @@ $( document ).ready(function () {
   let ls_amen = [];
 
   $("input[type='checkbox']").change (function () {
-    let checked = $(this).data('data-id');
+    let checked = $(this).attr('data-name');
       if ($(this).is(':checked')) {
-	ls_amen.push(checked);
+	ls_amen.push(" " + checked);
       } else {
-	ls_amen.splice($.inArray(checked, ls_amen), 1);
+	ls_amen.splice(checked, 1);
       }
     $('.amenities h4').text(ls_amen);
-
 
   });
 
