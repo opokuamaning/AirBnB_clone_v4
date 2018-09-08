781c75b87630b6961ec2c2df2fa91ec342a7f980
MelissaN <majajachiu@yahoo.com>
2018-09-08 20:26:14 +0000

    update javascript to have cumulative features

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,22 @@
 $( document ).ready(function () {
 
+  /*****************************************************
+    display list of checkboxes clicked
+   *****************************************************/
+  let ls_amen = [];
+  $("input[type='checkbox']").change (function () {
+    let checked = $(this).attr('data-name');
+      if ($(this).is(':checked')) {
+	ls_amen.push(" " + checked);
+      } else {
+	ls_amen.splice(checked, 1);
+      }
+    $('.amenities h4').text(ls_amen);
+  });
+
+  /*******************************************************
+    display red circle on top right of page if status ok
+   *******************************************************/
   $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
     if (textStatus === 'OK') {
       $('#api_status').addClass('available');
