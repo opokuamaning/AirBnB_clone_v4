af6986f2473030e9ed858243f28e5eceffc45840
MelissaN <majajachiu@yahoo.com>
2018-09-09 21:15:22 +0000

    add little house details within each article append

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -4,26 +4,29 @@ $( document ).ready(function () {
     display list of checkboxes clicked
    *****************************************************/
   let ls_amen = [];
-  $("input[type='checkbox']").change (function () {
-    let checked = $(this).attr('data-name');
+  $('input[type=checkbox]').change (function () {
+    let name = $(this).attr('data-name');
       if ($(this).is(':checked')) {
-	ls_amen.push(" " + checked);
+	ls_amen.push(name);
       } else {
-	ls_amen.splice(checked, 1);
+	ls_amen = ls_amen.filter(amen => amen !== name);
       }
-    $('.amenities h4').text(ls_amen);
+    $('.amenities h4').text(ls_amen.join(', '));
   });
 
   /*******************************************************
     display red circle on top right of page if status ok
    *******************************************************/
-  $.get('http://127.0.0.1:5002/api/v1/status/', function (data, textStatus) {
-    console.log(textStatus);
-    if (textStatus === 'OK') {
-      console.log(textStatus);
-      $('#api_status').addClass('available');
-    } else {
-      $('#api_status').removeClass('available');
+  $.ajax({
+    type: 'GET',
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    dataType: 'json',
+    success: function (data) {
+      if (data.status === 'OK') {
+	$('#api_status').addClass('available');
+      } else {
+	$('#api_status').removeClass('available');
+      }
     }
   });
 
@@ -31,32 +34,15 @@ $( document ).ready(function () {
     populate Places from frontend, instead of backend jinja
    *******************************************************/
     $.ajax({
-    type: 'POST',
-    url: 'http://0.0.0.0:5002/api/v1/places_search/',
-    data: '{}',
-    contentType: 'application/json',
-    success: function(data) {
-      for (let i = 0; i < data.length - 1; i++) {
-	    console.log(data[i]);
-	    $('section.places').append('<article>' + data[i] + '</article>');
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      data: JSON.stringify({}),
+      contentType: 'application/json',
+      success: function (data) {
+	for (let i = 0; i < data.length; i++) {
+	  $('section.places').append('<article><div class="title"><h2>' + data[i].name + '</h2><div class="price_by_night">' + data[i].price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + data[i].max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + data[i].number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + data[i].number_bathrooms + ' Bathroom</div></div><div class="description">' + data[i].description + '</div></article>');
+	}
       }
-    },
-    error: function(req, err) {
-      console.log(err);
-    }
-  });
+    });
+
 });
-
-/*  $.post('http://127.0.0.0:5002/api/v1/places_search/',
-	 {
-	   contentType: 'application/json',
-	   data: '{}'
-	 },
-	 function(data, textStatus) {
-	   for (let i = 0; i < data.length - 1; i++) {
-	     $('section.places').append('<article>' + data[i] + '</article>');
-	   }
-	 }
-	);
-
-}); */
