96c6c9c2fcbeedd905d81b2c62ca7e1e89de5cec
MelissaN <majajachiu@yahoo.com>
2018-09-09 02:15:19 +0000

    draft of ajax post function

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -17,23 +17,44 @@ $( document ).ready(function () {
   /*******************************************************
     display red circle on top right of page if status ok
    *******************************************************/
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+  $.get('http://127.0.0.1:5002/api/v1/status/', function (data, textStatus) {
     if (textStatus === 'OK') {
       $('#api_status').addClass('available');
     } else {
       $('#api_status').removeClass('available');
     }
-  });  
+  });
 
-  $.post('http://0.0.0.0:5001/api/v1/places_search/',
-  {
-    Content-Type: 'application/json',
-    data: {}
-  },
-  function(data, textStatus) {
-    for (let i = 0; i < data.length - 1; i++) {
-      $('section.places').append('<article>' + data[i] + '</article>');
+  /*******************************************************
+    populate Places from frontend, instead of backend jinja
+   *******************************************************/
+/*  $.ajax({
+    type: 'POST',
+    contentType: 'application/json',
+    url: 'http://127.0.0.1:5002/api/v1/places_search/',
+    data: '{}',
+    success: function(data) {
+      for (let i = 0; i < data.length - 1; i++) {
+	console.log(data[i]);
+	$('section.places').append('<article>' + data[i] + '</article>');
+      }
+    },
+    error: function(err) {
+      alert('error' + JSON.stringify(err));
     }
   });
+*/
+
+  $.post('http://127.0.0.0:5002/api/v1/places_search/',
+	 {
+	   contentType: 'application/json',
+	   data: '{}'
+	 },
+	 function(data, textStatus) {
+	   for (let i = 0; i < data.length - 1; i++) {
+	     $('section.places').append('<article>' + data[i] + '</article>');
+	   }
+	 }
+	);
 
 });
