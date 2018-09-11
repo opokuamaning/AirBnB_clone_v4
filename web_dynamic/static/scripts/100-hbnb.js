2ae43e606492adb8c8aaa03175d4a1bf39d009b9
MelissaN <majajachiu@yahoo.com>
2018-09-11 02:59:36 +0000

    add checkbox function to location filter; connect to search button

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -17,7 +17,7 @@ $( document ).ready(function () {
     display list of City checkboxes clicked
    *****************************************************/
   let ls_cities = {};
-  $('.locations ul.popover LI UL LI input[type=checkbox]').change (function () {
+  $('.locations ul.popover LI.c input[type=checkbox]').change (function () {
     if ($(this).is(':checked')) {
       ls_cities[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
@@ -30,7 +30,7 @@ $( document ).ready(function () {
     display list of State checkboxes clicked
    *****************************************************/
   let ls_states = {};
-  $('.locations ul.popover LI input[type=checkbox]').change (function () {
+  $('.locations ul.popover LI.s input[type=checkbox]').change (function () {
     if ($(this).is(':checked')) {
       ls_states[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
@@ -97,11 +97,15 @@ $( document ).ready(function () {
     $('article').remove();
     $.ajax({
       type: 'POST',
-      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      url: 'http://127.0.0.1:5002/api/v1/places_search/',
       data: JSON.stringify({'amenities': Object.keys(ls_amen), 'cities': Object.keys(ls_cities), 'states': Object.keys(ls_states)}),
       contentType: 'application/json',
       success: function (data) {
 	for (let i = 0; i < data.length; i++) {
+	  console.log(data[i]);
+	  console.log(ls_amen);
+	  console.log('cities: ', ls_cities);
+	  console.log('states: ', ls_states);
 	  $('section.places').append('<article><div class="title"><h2>' + data[i].name + '</h2><div class="price_by_night">' + data[i].price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + data[i].max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + data[i].number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + data[i].number_bathrooms + ' Bathroom</div></div><div class="description">' + data[i].description + '</div></article>');
 	}
       }
